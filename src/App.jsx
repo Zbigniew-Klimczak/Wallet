ed5532ca15def2a9ca88f33822e307b7ed86a150
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-07-18 15:44:07 +0200

    loginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
+const LoginPage = lazy(() => import("./pages/Login/Login"));
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import LoginForm from "../../components/loginForm/LoginForm";
+import css from "./Login.module.css";
+import bigLogo from "../../images/bigLogo.png";
+
+const Login = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+  return (
+    <>
+      {windowWidth < 768 && <LoginForm />}
+      {windowWidth >= 768 && windowWidth < 1280 && (
+        <main className={css.container}>
+          <div className={css.header}>
+            <img src={bigLogo} alt="Logo" className={css.logo} />
+            <h1 className={css.title}>Finance App</h1>
+          </div>
+          <div className={css.loginForm}>
+            <LoginForm />
+          </div>
+        </main>
+      )}
+      {windowWidth >= 1280 && (
+        <main className={css.container}>
+          <div className={css.header}>
+            <img src={bigLogo} alt="Logo" className={css.logo} />
+            <h1 className={css.title}>Finance App</h1>
+          </div>
+          <div className={css.loginFormBig}>
+            <LoginForm />
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default Login;
