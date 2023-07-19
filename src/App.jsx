9f5834cbe3f299b36da57e1980a09caf25887658
Dominik Lenart <dominik.lenart84@gmail.com>
2023-07-19 22:39:39 +0200

    Update Registration-Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-
-import "./App.css";
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import './App.css';
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 // import { useEffect } from "react";
 // import { useDispatch } from "react-redux";
 
-const LoginPage = lazy(() => import("./pages/Login/Login"));
-const RegistrationPage = lazy(() => import("./pages/Registration/Registration"));
-const ReduxExample = lazy(() => import("./pages/example/reduxExample"));
+const LoginPage = lazy(() => import('./pages/Login/Login'));
+
+const RegistrationPage = lazy(() =>
+	import('./pages/Registration/Registration')
+);
+const ReduxExample = lazy(() => import('./pages/example/reduxExample'));
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/redux" element={<ReduxExample />} />
-      </Routes>
-    </div>
-  );
-
+	return (
+		<div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route
+						path='/'
+						element={<LoginPage />}
+					/>
+					<Route
+						path='/register'
+						element={<RegistrationPage />}
+					/>
+					<Route
+						path='/redux'
+						element={<ReduxExample />}
+					/>
+				</Routes>
+			</Suspense>
+		</div>
+	);
 };
 
 export default App;
