1c3d0a6b1d7e5101547fc7f1010e2794cc75a359
Dominik Lenart <dominik.lenart84@gmail.com>
2023-07-19 21:31:14 +0200

    update

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -2,6 +2,11 @@
 import css from './LoginForm.module.css';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../redux/userSlice/userSlice';
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+
 // import { envelope } from "../../SVG/envelope.svg";
 const LoginForm = () => {
 	const SignupSchema = Yup.object().shape({
@@ -9,9 +14,32 @@ const LoginForm = () => {
 		password: Yup.string().required('Required'),
 	});
 
+	const dispatch = useDispatch();
+	const { user, token, error } = useSelector((state) => state.user);
+
+	useEffect(() => {
+		if (token) {
+			toast(`Hello ${user.email}`);
+		}
+		if (error !== null) {
+			toast('Zły email lub hasło');
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [token, error]);
+
 	return (
 		<div className={css.container}>
 			<div className={css.logo}>
+				<Toaster
+					toastOptions={{
+						style: {
+							border: '5px solid yellow',
+							fontSize: '36px',
+							backgroundColor: 'red',
+							color: '#fff',
+						},
+					}}
+				/>
 				<img
 					className={css.logo__icon}
 					src='/logo.svg'
@@ -25,9 +53,8 @@ const LoginForm = () => {
 					password: '',
 				}}
 				validationSchema={SignupSchema}
-				onSubmit={async (values) => {
-					await new Promise((r) => setTimeout(r, 500));
-					alert(JSON.stringify(values, null, 2));
+				onSubmit={(values) => {
+					dispatch(login(values));
 				}}
 			>
 				<Form className={css.form}>
@@ -41,9 +68,7 @@ const LoginForm = () => {
 						<ErrorMessage
 							name='email'
 							component='Field'
-						>
-							{(msg) => <div>{msg}</div>}
-						</ErrorMessage>
+						/>
 					</div>
 					<div className={css.input__div}>
 						<Field
@@ -56,9 +81,7 @@ const LoginForm = () => {
 						<ErrorMessage
 							name='password'
 							component='Field'
-						>
-							{(msg) => <div>{msg}</div>}
-						</ErrorMessage>
+						/>
 					</div>
 					<button
 						className={css.login}
