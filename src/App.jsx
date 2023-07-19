db70d67a517ce9e4dc58c204d15dc9d352d00a58
Dominik Lenart <dominik.lenart84@gmail.com>
2023-07-19 20:06:26 +0200

    Registration-Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import "./App.css";
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import './App.css';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-const LoginPage = lazy(() => import("./pages/Login/Login"));
+const LoginPage = lazy(() => import('./pages/Login/Login'));
+const RegistrationPage = lazy(() =>
+	import('./pages/Registration/Registration')
+);
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route
+					path='/'
+					element={<LoginPage />}
+				/>
+				<Route
+					path='/register'
+					element={<RegistrationPage />}
+				/>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
