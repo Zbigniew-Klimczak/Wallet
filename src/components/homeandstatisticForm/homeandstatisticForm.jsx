12a77c71a8777874dc1f91b5abd9863d2be22135
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-07-27 21:03:16 +0200

    Change /home /diagram navigation to NavLinks

--- src/components/homeandstatisticForm/homeandstatisticForm.jsx
+++ src/components/homeandstatisticForm/homeandstatisticForm.jsx
@@ -1,67 +1,62 @@
-import css from './homeAndStatisticForm.module.css';
-import Home from '../../images/home.png';
-import Statistics from '../../images/statistics.png';
-import ChartDoughnut from '../Chart/Chart';
+import css from "./homeAndStatisticForm.module.css";
+import Home from "../../images/home.png";
+import Statistics from "../../images/statistics.png";
+import ChartDoughnut from "../Chart/Chart";
+import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+
+const StyledNavLink = styled(NavLink)`
+  &.active {
+    font-family: "Poppins-Bold";
+  }
+`;
 
 function App() {
-	
-	const data = {
-		labels: [
-			'Income',
-			'Main expenses',
-			'Products',
-			'Car',
-			'Self care',
-			'Child care',
-			'Household products',
-			'Education',
-			'Leisure',
-			'Other expenses',
-			'Entertainment',
-		],
-	}
-	const colors = [
-		'#FED057',
-		'#FFD8D0',
-		'#FD9498',
-		'#C5BAFF',
-		'#6E78E8',
-		'#4A56E2',
-		'#81E1FF',
-		'#24CCA7',
-		'#00AD84',
-		'#784fca',
-	];
+  const data = {
+    labels: [
+      "Income",
+      "Main expenses",
+      "Products",
+      "Car",
+      "Self care",
+      "Child care",
+      "Household products",
+      "Education",
+      "Leisure",
+      "Other expenses",
+      "Entertainment",
+    ],
+  };
+  const colors = [
+    "#FED057",
+    "#FFD8D0",
+    "#FD9498",
+    "#C5BAFF",
+    "#6E78E8",
+    "#4A56E2",
+    "#81E1FF",
+    "#24CCA7",
+    "#00AD84",
+    "#784fca",
+  ];
 
-	const expense = 0;
+  const expense = 0;
 
-	return (
-		<div className={css.container}>
-			<div className={css.buttonContainer}>
-				<img
-					className={css.image}
-					src={Home}
-					alt='icon'
-				/>
-				<button className={css['home-button']}>Home</button>
-			</div>
-			<div className={css.buttonContainer}>
-				<img
-					className={css.image}
-					src={Statistics}
-					alt='icon'
-				/>
-				<button className={css['statistics-button']}>Statistics</button>
-			</div>
-			<div className={css.doughnutContainer}>
-				<ChartDoughnut
-					data={data}
-					colors={colors}
-					expense={expense}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className={css.container}>
+      <StyledNavLink to="/home" className={css.buttonContainer}>
+        <img className={css.image} src={Home} alt="icon" />
+        <button className={css["home-button"]}>Home</button>
+      </StyledNavLink>
+      <StyledNavLink to="/diagram" className={css.buttonContainer}>
+        <img className={css.image} src={Statistics} alt="icon" />
+        <button className={css["statistics-button"]}>Statistics</button>
+      </StyledNavLink>
+      <div className={css.doughnutContainer}>
+        <ChartDoughnut data={data} colors={colors} expense={expense} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
