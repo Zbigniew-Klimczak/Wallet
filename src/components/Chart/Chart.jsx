af64a1f78769ace16e52218a6d6c211273bffc27
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-07-29 13:30:07 +0200

    u

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,58 +1,87 @@
-import { Doughnut } from 'react-chartjs-2';
-import css from './Chart.module.css';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from "react-chartjs-2";
+import css from "./Chart.module.css";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
-const ChartDoughnut = ({ categories, colors, expense }) => {
-	ChartJS.register(ArcElement, Tooltip, Legend);
+const ChartDoughnut = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+  const token = useSelector((state) => state.user.token);
+  const balance = useSelector((state) => state.user.balance);
+  const [categories, setCategories] = useState([]);
+  const [money, setMoney] = useState([]);
+  const [allMoney, setAllMoney] = useState(0);
+  const yes = true;
 
-	const data = {
-		labels: categories || [],
-		datasets: [
-			{
-				label: '',
-				data: categories ? categories.map((_, index) => 8 - index) : [1],
-				backgroundColor:
-					categories && categories.length > 0 ? colors : ['#24cca7'],
+  const getCategories = async () => {
+    const response = await axios.get(
+      "https://wallet-backend-efx6.onrender.com/users/statistics/10/2022",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const keys = Object.keys(response.data.data);
+    const values = Object.values(response.data.data);
+    setCategories(keys);
+    setMoney(values);
+    console.log(values);
+    console.log(keys);
+    setAllMoney(values.reduce((acc, value) => acc + value, 0));
+  };
 
-				borderWidth: 0,
-				cutout: '68%',
-			},
-		],
-	};
+  const data = {
+    labels: allMoney === 0 ? ["no transactions"] : categories.slice(2, -1),
+    datasets: [
+      {
+        label: "",
+        data: allMoney === 0 ? [1] : money.slice(2, -1),
+        backgroundColor: [
+          "#FED057",
+          "#FFD8D0",
+          "#FD9498",
+          "#C5BAFF",
+          "#6E78E8",
+          "#4A56E2",
+          "#81E1FF",
+          "#24CCA7",
+          "#00AD84",
+        ],
 
-	const options = {
-		plugins: {
-			legend: {
-				display: false,
-			},
-		},
-		responsive: true,
-		devicePixelRatio: 2,
-	};
+        borderWidth: 0,
+        cutout: "68%",
+      },
+    ],
+  };
 
-	return (
-		<div className={css.container}>
-			{expense ? (
-				<>
-					<Doughnut
-						data={data}
-						options={options}
-						className={css.doughnut}
-					/>
-					<p className={css.expense}>€{expense}</p>
-				</>
-			) : (
-				<>
-					<Doughnut
-						data={data}
-						options={options}
-						className={css.doughnut}
-					/>
-					<p className={css.expense}>No expenses</p>
-				</>
-			)}
-		</div>
-	);
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    responsive: true,
+    devicePixelRatio: 2,
+  };
+
+  return (
+    <div className={css.container}>
+      <button onClick={getCategories}>fetch</button>
+      {yes ? (
+        <>
+          <Doughnut data={data} options={options} className={css.doughnut} />
+          <p className={css.expense}>€{balance}</p>
+        </>
+      ) : (
+        <>
+          <Doughnut data={data} options={options} className={css.doughnut} />
+          <p className={css.expense}>No expenses</p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ChartDoughnut;
