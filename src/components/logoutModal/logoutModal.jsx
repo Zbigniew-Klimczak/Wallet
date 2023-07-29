c369f4a5dda0b7a31beffd5ffac4cedeffa915d6
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-07-29 17:59:08 +0200

    Added close modal on outside click or esc key

--- src/components/logoutModal/logoutModal.jsx
+++ src/components/logoutModal/logoutModal.jsx
@@ -1,21 +1,32 @@
 import css from "./logoutModal.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { logout, setLogoutModal } from "../../redux/userSlice/userSlice";
+import { useEffect } from "react";
 
 const LogoutModal = () => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.user);
+  const handleLogoutModal = () => {
+    dispatch(setLogoutModal(false));
+  };
   const handleLogout = () => {
     dispatch(logout(token));
     handleLogoutModal();
   };
-  const handleLogoutModal = () => {
-    dispatch(setLogoutModal(false));
-  };
+
+  useEffect(() => {
+    const close = (e) => {
+      if (e.key === "Escape") {
+        handleLogoutModal();
+      }
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
 
   return (
-    <div className={css.container}>
-      <div className={css.modal}>
+    <div className={css.container} onClick={handleLogoutModal}>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
         <p className={css.text}>Are you sure you want to log out?</p>
         <div className={css.buttonContainer}>
           <button className={css.button} onClick={handleLogout}>
