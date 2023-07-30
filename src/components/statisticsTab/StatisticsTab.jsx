01244b8574418ecc86232713757ee54a2f74f045
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-07-30 10:47:58 +0200

    tabelka statystyki

---
+++ src/components/statisticsTab/StatisticsTab.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import css from "./statisticsTab.module.css";
+
+const StatisticsTab = () => {
+  const token = useSelector((state) => state.user.token);
+  const [categories, setCategories] = useState([]);
+  const [money, setMoney] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [incomes, setIncomes] = useState([]);
+
+  const colors = [
+    "#FED057",
+    "#FFD8D0",
+    "#FD9498",
+    "#C5BAFF",
+    "#6E78E8",
+    "#4A56E2",
+    "#81E1FF",
+    "#24CCA7",
+    "#00AD84",
+  ];
+
+  useEffect(() => {
+    getCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getCategories = async () => {
+    const response = await axios.get(
+      "https://wallet-backend-efx6.onrender.com/users/statistics/10/2022",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const keys = Object.keys(response.data.data);
+    const values = Object.values(response.data.data);
+    setCategories(keys.slice(2, -1));
+    setMoney(values.slice(2, -1));
+    setExpenses(values[0]);
+    setIncomes(values[1]);
+    console.log(values);
+    console.log(keys);
+    // setAllMoney(values.reduce((acc, value) => acc + value, 0));
+  };
+
+  return (
+    <div className="statisticsTab">
+      <div className={css.header}>
+        <p className={css.header__text}>Category</p>
+        <p className={css.header__text}>Sum</p>
+      </div>
+      <div>
+        <ul className={css.list}>
+          {categories.map((category, index) => (
+            <li className={css.listItem} key={index}>
+              <div className={css.categoryAndColor}>
+                <div
+                  className={css.squere}
+                  style={{ backgroundColor: `${colors[index]}` }}
+                ></div>
+                <p>{category}</p>
+              </div>
+              <p>{money[index]}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={css.expenses}>
+        <p className={css.expenses__text}>Expenses:</p>
+        <p className={`${css.expenses__text} ${css.expenses__red}`}>
+          {expenses}
+        </p>
+      </div>
+      <div className={css.expenses}>
+        <p className={css.expenses__text}>Income:</p>
+        <p className={`${css.expenses__text} ${css.expenses__green}`}>
+          {incomes}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsTab;
