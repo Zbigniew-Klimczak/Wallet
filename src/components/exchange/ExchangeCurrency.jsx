e7dfccd41bad2c078c095df7778f62a86be4949e
Dominik Lenart <dominik.lenart84@gmail.com>
2023-08-02 00:28:06 +0200

    ExchangeCurrency fetch from API

---
+++ src/components/exchange/ExchangeCurrency.jsx
@@ -0,0 +1,23 @@
+const requestURL = 'https://api.exchangerate.host/latest';
+
+// Retrieve the data from localStorage
+const savedData = JSON.parse(localStorage.getItem('data') || null);
+const currentTimestamp = new Date().getTime();
+
+if (savedData && currentTimestamp - savedData.timestamp < 3600000) {
+	// less than one hour
+	console.log('Using saved data:', savedData.data);
+} else {
+	fetch(requestURL)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+
+			// Save the response and the current timestamp in localStorage
+			const timestamp = new Date().getTime();
+			localStorage.setItem('data', JSON.stringify({ data, timestamp }));
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+}
