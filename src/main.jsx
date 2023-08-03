33891db26e5bb67fd1db46b964f7c1f19a9fed5e
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-08-03 17:43:38 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="/Wallet">
           <App />
         </BrowserRouter>
       </PersistGate>
