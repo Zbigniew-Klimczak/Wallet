ed438c8e664896058485f0ff4905285ef8eedd49
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-08-03 16:41:16 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/Wallet/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
