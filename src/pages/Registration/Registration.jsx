54d997a4d9fab880fb952e46104df9bf18225701
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-08-03 18:07:59 +0200

    Logo

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,52 +1,44 @@
-import { useState, useEffect } from 'react';
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
-import css from './Registration.module.css';
-import bigLogo2 from '../../images/bigLogo2.png';
+import { useState, useEffect } from "react";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import css from "./Registration.module.css";
+import bigLogo2 from "../../images/headerLogo.png";
 
 const Registration = () => {
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-	useEffect(() => {
-		const handleResize = () => setWindowWidth(window.innerWidth);
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, [windowWidth]);
-	return (
-		<>
-			{windowWidth < 768 && <RegistrationForm />}
-			{windowWidth >= 768 && windowWidth < 1280 && (
-				<main className={css.container}>
-					<div className={css.header}>
-						<img
-							src={bigLogo2}
-							alt='Logo'
-							className={css.logo}
-						/>
-						<h1 className={css.title}>Finance App</h1>
-					</div>
-					<div className={css.registrationForm}>
-						<RegistrationForm />
-					</div>
-				</main>
-			)}
-			{windowWidth >= 1280 && (
-				<main className={css.container}>
-					<div className={css.header}>
-						<img
-							src={bigLogo2}
-							alt='Logo'
-							className={css.logo}
-						/>
-						<h1 className={css.title}>Finance App</h1>
-					</div>
-					<div className={css.registrationFormBig}>
-						<RegistrationForm />
-					</div>
-				</main>
-			)}
-		</>
-	);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+  return (
+    <>
+      {windowWidth < 768 && <RegistrationForm />}
+      {windowWidth >= 768 && windowWidth < 1280 && (
+        <main className={css.container}>
+          <div className={css.header}>
+            <img src={bigLogo2} alt="Logo" className={css.logo} />
+            <h1 className={css.title}>Finance App</h1>
+          </div>
+          <div className={css.registrationForm}>
+            <RegistrationForm />
+          </div>
+        </main>
+      )}
+      {windowWidth >= 1280 && (
+        <main className={css.container}>
+          <div className={css.header}>
+            <img src={bigLogo2} alt="Logo" className={css.logo} />
+            <h1 className={css.title}>Finance App</h1>
+          </div>
+          <div className={css.registrationFormBig}>
+            <RegistrationForm />
+          </div>
+        </main>
+      )}
+    </>
+  );
 };
 
 export default Registration;
